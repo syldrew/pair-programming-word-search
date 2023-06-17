16b58ec6b942a333ca5bd5779f180bd8647ff47e
syldrew <syl_peke14@hotmail.com>
2023-06-17 18:55:09 -0500

    PairProgrammin WordSearch Rabia ,Tolu and Silvia

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
+const transpose = (matrix) => {
+    let newMatrix = [];
+    for (let i = 0; i < matrix[0].length; i++){
+        newMatrix.push([])
+        for (let j = 0; j < matrix.length; j++) {
+            newMatrix[i][j] = matrix[j][i]
+        }
+    }
+    return newMatrix
+}
+
+const wordSearch = (letters, word) => {
+    if (letters.length === 0) return false
+    
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
     const horizontalJoin = letters.map(ls => ls.join(''))
+    
+
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }
+    return false
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
